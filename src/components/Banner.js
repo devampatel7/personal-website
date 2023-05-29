4098ea47468513778789a268a8d28fa8d60153c4
Devam Patel <devampatel7@gmail.com>
2023-05-28 22:08:10 -0400

    modified paragraph, linked in link on navbar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "Team Leader" ];
+  const toRotate = [ "software engineer", "data scientist" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,11 +56,10 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Devam,`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "Team Leder" ]'><span className="wrap">{text}</span></span></h1>
-                <p>My fullname is Saidnazarova Odinanabonu. I'm 15 years old. I study 249-school in 10th grade.I am a front-end web developer. I can make the website more, more interactive with web animation
-           I study at Web Brain Academy group G5. When I first came to web brain, I had no knowledge of programming.
-           But now with the help of strong aspirations and teachers, my level of knowledge has greatly increased and I can freely create web site views that are used in our daily lives
-        </p>
+                <h1>{`Hi! I'm Devam, an aspiring`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "software engineer", "data engineer", "team leader" ]'><span className="wrap">{text}</span></span></h1>
+                <p> 
+                Welcome to my personal website! As a dedicated student currently pursuing my degree in computer science at Rutgers University, I am determined to make my mark in the tech industry and learn more in the process. Join me as I embark on this journey, constantly seeking growth and aiming to make a meaningful impact in the tech world.
+                </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,6 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/odina2.png';
 import '../styling/navbar.css';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -54,9 +52,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/devam-patel-ddp7/"><img src={navIcon1} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>

96607531320c0ee54dcd5fa151034b165203adba
Devam Patel <devampatel7@gmail.com>
2023-05-29 14:32:47 -0400

    changed banner pics and fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,8 @@ import {
   BrowserRouter as Router
 } from "react-router-dom";
 
+
+
 export const NavBar = () => {
 
   const [activeLink, setActiveLink] = useState('home');
@@ -36,17 +38,13 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <div className="navbar-home">
-                <h1>Home</h1>
-            </div>
-           
+          <span className="brand" onClick={() => onUpdateActiveLink('home')}>Home</span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
